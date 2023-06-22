3a3d74bb744c8b286e2a748f09111acb9446879e
705105070 <tranlinh16982@gmail.com>
2023-06-22 23:49:02 +0700

    updete

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tran Linh
+          Tran Thuy Linh
         </p>
       </header>
     </div>
